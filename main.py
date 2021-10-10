c6ae8527ee56e9194203b6d1257ca34eabff4eab
Linus Romland <hello@linusromland.com>
2021-10-10 16:43:58 +0200

    Sets wallpaper to top of combined subs

--- main.py
+++ main.py
@@ -1,3 +1,34 @@
 import os
+import requests
+import pathlib
 
-os.system("/usr/bin/gsettings set org.gnome.desktop.background picture-uri ~/Pictures/coolWallpaper.jpg")
+text_file = open("./subreddits.txt", "r")
+rawSubreddits = text_file.read()
+text_file.close()
+
+subreddits = rawSubreddits.split("\n")
+combinedSubs = ""
+
+for i in range(len(subreddits)):
+    combinedSubs += subreddits[i]
+    if(i < len(subreddits) -1):
+        combinedSubs += "+"
+
+
+redditData = requests.get(f'https://www.reddit.com/r/{combinedSubs}/top.json', headers = {'User-agent': 'AutomateWallpaper'})
+
+redditJSON = redditData.json()["data"]["children"]
+index = 0
+
+while True:
+    imageURL = redditJSON[index]["data"]["url"]
+    if imageURL[-4:] == ".jpg":
+        break
+    index = index + 1
+
+image = requests.get(imageURL, allow_redirects=True)
+open('wallpaper.jpg', 'wb').write(image.content)
+
+print(redditData.headers["x-ratelimit-remaining"])
+os.system(f"/usr/bin/gsettings set org.gnome.desktop.background picture-uri {str(pathlib.Path().resolve())}/wallpaper.jpg")
+
